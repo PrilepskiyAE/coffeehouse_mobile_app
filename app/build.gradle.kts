0aa5b1e9539077cdc7a5caf840ca4180c461cf45
Prilepskiy_AE <woolf7754286@yandex.ru>
2024-01-02 21:12:21 +0700

    add dept

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,23 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+    id("kotlinx-serialization")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.coffeehouse.ch_mobile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.coffeehouse.ch_mobile"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -30,11 +35,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -42,7 +47,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
-    packaging {
+    packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
@@ -50,20 +55,30 @@ android {
 }
 
 dependencies {
+    implementation(Dependencies.Core.coreKtx)
+    implementation(Dependencies.Kotlin.serialization)
+    implementation(Dependencies.Lifecycle.runtime)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Dependencies.Compose.animation)
+    implementation(Dependencies.Compose.foundation)
+    implementation(Dependencies.Compose.material)
+    implementation(Dependencies.Compose.material3)
+    implementation(Dependencies.Compose.runtime)
+    implementation(Dependencies.Compose.ui)
+    implementation(Dependencies.Compose.toolingPreview)
+
+    implementation(Dependencies.Navigation.hiltNavigation)
+    implementation(Dependencies.Navigation.composeNavigation)
+
+    implementation(Dependencies.Hilt.android)
+    kapt(Dependencies.Hilt.compiler)
+
+    implementation(Dependencies.Test.junit)
+    implementation(Dependencies.Test.extJunit)
+    implementation(Dependencies.Test.espresso)
+    implementation(Dependencies.Test.junit)
+    implementation(Dependencies.Test.extJunit)
+    implementation(Dependencies.Test.espresso)
+    implementation(Dependencies.Biometric.biometric)
+    implementation(Dependencies.Coil.coil)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "coffeehouse mobile app"
 include(":app")
- 
